631ba6af1ba2bb882d78ba7684127507e0f20816
Robin Scholtes <robin@theweirdscience.com>
2021-12-23 00:00:41 +1300

    first version

--- index.js
+++ index.js
@@ -7,24 +7,45 @@ import { decode } from './commands/decode.js';
 
 program
     .option( '-e, --encode', 'encode to dolphin')
-    .argument('<input>')
-    .action(( input ) => {
-        console.log( Chalk.cyan.bold( encode( input )));
-    })
-
-program
     .option('-d, --decode', 'decode from dolphin')
-    .argument('<input>')
+    .option('-D, --dolphin', 'see a dolphin')
+    .argument('[input]')
     .action(( input ) => {
-        console.log( Chalk.cyan.bold( decode( input )));
+        if ( program.opts().encode ) {
+            console.log( Chalk.cyan.bold( encode( input )));
+        } else if ( program.opts().decode ) {
+            console.log( Chalk.cyan.bold( decode( input )));
+        } else if ( program.opts().dolphin ) {
+            console.log( Chalk.bold(`              
+                                  __         
+                               _.-~  )
+                    _..--~~~~,'   ,-/     _
+                 .-' . . . .'   ,-','    ,' )
+               ,' . . . _   ,--~,-'__..-'  ,'
+             ,' . . .  (@)' ---~~~~      ,'
+            / . . . . '~~             ,-'
+           / . . . . .             ,-'
+          ;  . . . .  - .        ,'
+         :  . . . .       _     /
+        .  . . . .          \`-.:
+       .  . . ./  - .          )
+      .   . . |  _____..---.._/ ____ Seal _
+~---~~~~----~~~~             ~~
+
+Special shout-out to（✿ ͡◕ ᴗ◕)つ━━✫・*。
+${Chalk.hex('#CA09BC').bold('EEEEEEEEEeEeEeEE')}${Chalk.hex('#A318BF').bold('EEEEEEEEEeeEeeee')}${Chalk.hex('#7C27C2').bold('EEEEEEEEEeeEEEEe')}${Chalk.hex('#5535C5').bold('EEEEEEEEEeeeEEee')}${Chalk.hex('#2E44C8').bold('EEEEEEEEEeeeEeEE')}${Chalk.hex('#0753CB').bold('EEEEEEEEEeeEEeEe')}${Chalk.hex('#1478e3').bold('EEEEEEEEEeeeEEeE')}`))
+        } else {
+            console.log('Uh oh, command not recognized. Check out -h or --help for assistance');
+        }
     })
-
-program
     .version('1.0.0')
-    .addHelpText('after', Chalk.cyan(`
-dolphin8 -e "encode this string" 
-dolphin8 -x -d "decode this string"
-dolphin8 --decode "decode this string" 
-`))
+    .addHelpText('before', `
+https://dolphinonkeys.com for ଘ(੭ºัᴗºั)━☆ﾟ*:. 
+${Chalk.bgBlueBright(' °˖✧ The Dolphinator ✧˖° ')}${Chalk.bgMagentaBright(' online ')}
+`)
+    .addHelpText('after', `
+${Chalk('Example usage:')}
+${Chalk.blue.bold('dolphin8 -e "encode this string"')}
+${Chalk.blue.bold('dolphin8 --decode "EEEEEEEEEeeEeEEEEEEEEEEEEeeEeEEe"')}`)
 
-program.parse( process.argv );
+program.parse();
